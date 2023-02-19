194f30ecdd86eb6ebb421b11e6a117b799b86b0b
Awildernessboy <aidandclark@gmail.com>
2023-02-19 12:05:02 -0700

    new file:   .vscode/tasks.json new file:   calculator.cpp new file:   calculator.exe

---
+++ calculator.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    // variables go here
+    int a, b;
+    int sum = 0;
+
+    // inputs go here
+    cout << "Enter first Number : ";
+    cin >> a;
+    cout << "Enter second Number : ";
+    cin >> b;
+
+    // calculations go here
+    sum = a + b;
+
+    //outputs go here
+    cout << "Addition of two numbers is : " << sum;
+
+    return 0;
+}
